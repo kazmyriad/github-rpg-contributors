125e47807112d63330fec6e292592723aa883be3
Alyssa F <aiepillios1101@gmail.com>
2025-03-20 20:05:49 -0400

    Cleaned up CSS + added org/repo links

--- github-rpg-contributors.js
+++ github-rpg-contributors.js
@@ -70,33 +70,64 @@ export class GithubRpgContributors extends DDDSuper(I18NMixin(LitElement)) {
       .wrapper
       {
         text-align: center;
-        width: 100%;
+        width: 90vw;
       }
 
       .info
       {
         font-size: var(--ddd-font-size-4xs);
-        
       }
+
+      .footer{
+        text-align:center;
+        font-size: var(--ddd-font-size-xs);
+        color: var(--ddd-theme-default-slateGray);
+        padding: var(--ddd-spacing-5);
+      }
+
+      p
+      {
+        margin-bottom: var(--ddd-spacing-2);
+        color: var(--ddd-theme-default-creekTeal);
+      }
+
+      h4
+      {
+        margin: var(--ddd-spacing-0);
+      }
+
       .rpg-character
       {
         overflow: hidden;
       }
 
-      .rpg-character:hover
+      .info
       {
-      transition: all .5s ease;
-      transform: rotateY(180deg);
+        font-weight: var(--ddd-font-weight-bold);
       }
 
       .rpg-block
       {
         display:inline-block; 
-        width: 7em;
-        margin: 5px;
-        padding: 5px;
+        width: 9em;
+        margin: var(--ddd-spacing-2);
+        padding: var(--ddd-spacing-1);
         text-align: center;
+        border-radius: var(--ddd-radius-xl);
+        border: var( --ddd-border-lg);
+        background-color: var(--ddd-theme-default-navy65);
+        border-color: var(--ddd-theme-default-potential75);
+        transition: 1s;
       }
+
+      .rpg-block:hover
+      {
+        transition: all .8s ease-in-out;
+        color: var(--ddd-theme-default-discoveryCoral);
+        background-color: var(--ddd-theme-default-slateGray);
+        transform: rotateY(360deg);
+      }
+
       
     `];
   }
@@ -107,27 +138,32 @@ export class GithubRpgContributors extends DDDSuper(I18NMixin(LitElement)) {
     return html`
     <div class="wrapper"> 
           <!-- item.whatever ONLY workd within the map because item is defined here -->
+          <h2>Meet the Contributors for 
+            <a href="https://github.com/${this.org}/${this.repo}">${this.org}</a>
+          </h2>
           ${this.items.slice(0, this.limit).map((item, index) => html`
            
-            <a class="rpg-block" href="${item.html_url}" target="_blank">
+            <div class="rpg-block">
               <div class="rpg-character">
-                <rpg-character seed="${item.login}"></rpg-character>
+                <a class="character" href="${item.html_url}" target="_blank">
+                  <rpg-character seed="${item.login}"></rpg-character>
+                </a>
               </div>
+            
               <div class="info">
-                <p>${item.login}</p>
-                <p>Contributions: ${item.contributions}</p>
+                <h4>${item.login.length > 14 ? item.login.substring(0, 13) + "..." : item.login}</h4>
+                <p>${item.contributions} Contributions</p>
               </div>
-           </a>
+          </div>
           `)} 
 
-          <div class="footer">
-            <p>Showing the top ${this.limit} contributors. Click here to see them all!</p>
-          </div>
+          <p class="footer"> Showing the top ${this.limit} contributors. See the full list <a href="https://api.github.com/repos/${this.org}/${this.repo}/contributors">Here</a></p>
   </div>
    
     `;
     
   }
+
 
   updated(changedProperties) {
     //want this to run if more contributors are added. how to do.
